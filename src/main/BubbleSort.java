51a8437465d272a23157bbf3804216538b8ad21f
Ivan Krstev <ivankrstev246@gmail.com>
2024-07-23 22:28:20 +0200

    Added bubble sort algorithm with separate tests

---
+++ src/main/BubbleSort.java
@@ -0,0 +1,15 @@
+package main;
+
+public class BubbleSort {
+    public void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int k = 0; k < i; k++) {
+                if (arr[i] < arr[k]) {
+                    int temp = arr[i];
+                    arr[i] = arr[k];
+                    arr[k] = temp;
+                }
+            }
+        }
+    }
+}
